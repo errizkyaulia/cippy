c0980c72244b6cb5ae9afffa947ae5d4c9c0604f
errizky <errizkyaulia@gmail.com>
2026-07-27 23:02:38 +0700

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,82 @@
+import type { Metadata } from "next";
+import { Poppins, Rubik } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
+import { portfolioConfig } from "@/config/portfolio.config";
+import Image from "next/image";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-poppins",
+});
+const rubik = Rubik({
+  subsets: ["latin"],
+  weight: "600",
+  variable: "--font-rubik",
+});
+
+export const metadata: Metadata = {
+  metadataBase: new URL(portfolioConfig.seo.url),
+  title: {
+    default: portfolioConfig.name,
+    template: `%s - ${portfolioConfig.title}`,
+  },
+  description: portfolioConfig.description,
+
+  // added new keywords for seo
+  keywords: portfolioConfig.seo.keywords,
+  authors: portfolioConfig.seo.authors,
+  creator: portfolioConfig.name,
+
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: portfolioConfig.seo.url,
+    title: portfolioConfig.name,
+    description: portfolioConfig.description,
+    images: [`${portfolioConfig.seo.url}/og-image.png`],
+    siteName: portfolioConfig.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: portfolioConfig.name,
+    description: portfolioConfig.description,
+    images: [`${portfolioConfig.seo.url}/og-image.png`],
+    creator: portfolioConfig.seo.twitterHandle,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} ${rubik.variable}`}>
+        <main
+          className={cn(
+            "flex  relative  break-words h-dvh min-h-screen items-center justify-between pt-14 pb-4 px-40 max-md:p-4",
+            //{ "bg-white": "#E6E7EB" }
+          )}
+        ><Image
+            src="/hero-bg.png"
+            alt="Background"
+            fill
+            priority
+            className="object-cover -z-20"
+          />
+          
+          {/* NAVBAR ->  */}
+          <Navbar />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ src/config/portfolio.config.ts
@@ -0,0 +1,236 @@
+export const portfolioConfig = {
+  // Basic Information
+  name: "Assyifa Syafrita",
+  title: "Biology Master",
+  description: "I am a passionate biology master with a deep understanding of the natural world. I specialize in various biological fields and have a keen interest in research and education. My goal is to contribute to the scientific community through innovative research and effective teaching.",
+  location: "Jakarta, Indonesia",
+
+  // Contact Information
+  email: "syifasyafrita@gmail.com", // Replace with actual email if available
+
+  // Social Media Links
+  socialLinks: {
+    facebook: "",
+    twitter: "",
+    linkedin: "https://linkedin.com/in/assyifasyafrita",
+    external: "",
+    github: "", // Assuming this is the GitHub link
+  },
+
+  // SEO Information
+  seo: {
+    ogImage: "https://mdtaquiimam.vercel.app/og-image.png",
+    url: "https://mdtaquiimam.vercel.app",
+    twitterHandle: "",
+    keywords: [
+      
+    ],
+    authors: [
+      {
+        name: "Taqui Imam",
+        url: "https://github.com/taqui-786",
+      },
+    ],
+  },
+
+  // About Information
+  about: {
+    bio: "Simplicity.",
+    hobbies: [
+      "",
+      "",
+      "",
+      "",
+      "",
+    ],
+    personalInfo: {
+      language: "Native Indonesian, Fluent English",
+      nationality: "Indonesia",
+      gender: "Female",
+    },
+  },
+
+  // Skills and Roles
+  skills: {
+    roles: [
+      "Microbiology",
+      "Molecular",
+      "Anatomical & Clinical Pathology",
+    ],
+    // Technical Skills
+    programmingLanguages: [
+      {
+        name: "HTML5",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+      },
+      
+    ],
+    frameworks: [
+      {
+        name: "React",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+      },
+      
+    ],
+    tools: [
+      {
+        name: "Git",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+      },
+      
+    ],
+  },
+
+  // Education Information
+  education: [
+    {
+      degree: "Master of Science in Biology",
+      institution: "ANDALAS UNIVERSITY",
+      location: "Padang, Sumatera Barat",
+      period: "July 2023 – July 2024",
+      description:
+        "I am currently Studying Bachelor of Computer Application form Doranda College Ranchi a Goverment College of Ranchi. The program has provided me with a well-rounded education, covering both theoretical foundations and practical applications of computer science.",
+    },
+    {
+      degree: "Bachelor of Science in Biology",
+      institution: "ANDALAS UNIVERSITY",
+      location: "Padang, Sumatera Barat",
+      period: "August 2019 – Juli 2023",
+      description:
+        "I have completed my Bachelor of Science in Biology from ANDALAS UNIVERSITY. During my undergraduate studies, I gained a solid understanding of biological concepts and research methodologies. I actively participated in laboratory work, field studies, and academic projects, which enhanced my practical skills and knowledge in the field of biology.",
+    },
+  ],
+
+  // Projects Information
+  projects: [
+    {
+      title: "WePost- A modern, minimalistic social platform.",
+      description:
+        "WePost is a modern, minimalistic social platform for sharing posts, like, follow, and engaging in comments etc.",
+      tags: ["Tanstack-Query", "PostgreSQL", "Shadcn Ui", "Nextjs"],
+      link: "https://we-post-dev.vercel.app",
+    },
+    {
+      title: "Devzone- A community chat app for devs",
+      description:
+        "DevZone is the ultimate chat platform for developers to connect, collaborate, and code together. Join the conversation and level up your coding journey.",
+      tags: ["Zustand", "Supabase", "Shadcn Ui", "Nextjs"],
+      link: "https://devzone-ti.vercel.app/",
+    },
+    {
+      title: "itZmyLink- One Page many Links",
+      description:
+        "itZmyLink is a simple platform where user can create a personalized page to showcase all your social media profiles in one place. ",
+      tags: ["Nextjs", "Typescript", "Shadcn Ui"],
+      link: "https://github.com/taqui-786/itZmyLink",
+    },
+    {
+      title: "GitEstimate- Github estimate worth generator",
+      description:
+        "GitEstimate is a simple fun tool where user can generate their github estimate worth card just by entering their github username. ",
+      tags: ["Nextjs", "Typescript", "Shadcn Ui"],
+      link: "https://github.com/taqui-786/GitEstimate",
+    },
+    {
+      title: "Mixcn-ui- Reusable components for Nextjs",
+      description:
+        "This is Nextjs app with a Collection of Nextjs Components - (Currently under Devlelopment) ",
+      tags: ["Nextjs", "Shadcnui", "Npx", "Library"],
+      link: "https://mixcn-ui.vercel.app",
+    },
+    {
+      title: "Rupeespot- All deals Product in one place",
+      description:
+        "This is a free tool to check price history charts for millions of products for popular Indian stores such as Amazon and Flipkart. ",
+      tags: ["Nextjs", "Freelancing", "MySQL"],
+      link: "https://rupeespot.com/",
+    },
+    {
+      title: "Formcraft- Build Type-Safe Forms with Zero Effort",
+      description:
+        "Create production-ready forms with Zod validation, React Hook Form, and TypeScript. Design, customize, and deploy in minutes.",
+      tags: ["Nextjs", "Shadcnui", "Zod", "React Hook Form "],
+      link: "https://formcraft-ti.vercel.app/",
+    },
+    {
+      title: "KryptoKapital- Investing in Cryptocurreny",
+      description:
+        "This is a platform for learning about cryptocurrency with many tools and festures. It is very big project. ",
+      tags: ["Nextjs", "Freelancing", "Supabase"],
+      link: "https://pro.kryptokapital.info/",
+    },
+    {
+      title: "FriendZ - A social media Platform",
+      description:
+        "FriendZ is a social media app made with modern tech stacks such as redis, Authjs etc. A user can Create, delete, like, comment Post. ",
+      tags: ["Redis", "Authjs", "Typescript", "Nextjs"],
+      link: "https://github.com/taqui-786/project-friendz",
+    },
+    {
+      title: "ChatApp- Simple instagram clone",
+      description:
+        "ChatApp is similar to instagram, you can create, edit, delete, like,comment, post and  Follow, unfollow users",
+      tags: ["Reactjs", "Javascript"],
+      link: "https://github.com/taqui-786/chatapp-api",
+    },
+    {
+      title: "Devletter - A Newsletter for devs",
+      description:
+        "Devletter ia a newsletter subscribing single page app made with Nextjs and Tailwindcss.",
+      tags: ["Nextjs", "Tailwindcss"],
+      link: "https://github.com/taqui-786/Devletter",
+    },
+    {
+      title: "CrouMaker - A Crousal Maker App",
+      description:
+        "Crousal Maker is a tool with in-built crousals templates edit and download it in any format.",
+      tags: ["Nextjs", "jsPDF", "html2canvas", "Shadcn Ui"],
+      link: "https://github.com/taqui-786/crousal-maker",
+    },
+  ],
+
+  // More Links Information
+  moreLinks: [
+    {
+      title: "Dev.to",
+      description:
+        "I write blogs on web development, trending tech stacks or javascript guide or tips in Dev.to ",
+      link: "https://dev.to/random_ti",
+    },
+    {
+      title: "Hashnode",
+      description:
+        "I write blogs on web development, trending tech stacks or javascript guide or tips in Hashnode",
+      link: "https://mdtaquiimam.hashnode.dev/",
+    },
+    {
+      title: "Medium",
+      description:
+        "I write blogs on web development, trending tech stacks or javascript guide or tips in Medium",
+      link: "https://medium.com/@mdtaqui.jhar",
+    },
+    {
+      title: "Daily.Dev",
+      description:
+        "I am also the member of Daily Dev squads and i also upload post on some squads.",
+      link: "https://app.daily.dev/taqui_786",
+    },
+    {
+      title: "Gumroad",
+      description:
+        "I also sell digital products on gumroad like Notion Templates and web projects etc.",
+      link: "https://mdtaquijhar.gumroad.com/",
+    },
+    {
+      title: "Buy Me a coffee",
+      description:
+        "Supports my content quality, new topics, and project creation.",
+      link: "https://www.buymeacoffee.com/taquidevloper",
+    },
+  ],
+
+  // API Keys (should be in .env but referenced here)
+  apiKeys: {
+    resendApiKey: process.env.RESEND_API_KEY, // This should be loaded from environment variables in a real app
+  },
+};
